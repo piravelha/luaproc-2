91bbfd3a768a702d51a1d4ffe635f993e72d9103
Pira Loco <piralocojacavelha@gmail.com>
2024-08-22 16:20:24 -0300

    Everything

---
+++ src/lexer.rs
@@ -0,0 +1,118 @@
+use regex::Regex;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenKind {
+    Number,
+    String,
+    Boolean,
+    Nil,
+    Name,
+    Special,
+    Delimiter,
+    Brace,
+    Macro,
+    Define,
+    EndDefine,
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub value: String,
+}
+
+pub type Patterns = Vec<(Regex, TokenKind)>;
+pub type Tokens = Vec<Token>;
+
+fn new_pattern(pattern: &str) -> Regex {
+    Regex::new(&("^".to_string() + pattern)).unwrap()
+}
+
+fn get_lex_patterns() -> Patterns {
+    vec![(
+        new_pattern(r"-?\d+(\.\d+)?"),
+        TokenKind::Number,
+    ), (
+        new_pattern(r#""([^"\\]|\\.)*""#),
+        TokenKind::String,
+    ), (
+        new_pattern(r"(true|false)"),
+        TokenKind::Boolean,
+    ), (
+        new_pattern(r"([a-zA-Z_]\w*!)"),
+        TokenKind::Macro,
+    ), (
+        new_pattern(r"(#define)"),
+        TokenKind::Define,
+    ), (
+        new_pattern(r"(#end)"),
+        TokenKind::EndDefine,
+    ), (
+        new_pattern(r"(nil)"),
+        TokenKind::Nil,
+    ), (
+        new_pattern(r"([a-zA-Z_]\w*)"),
+        TokenKind::Name,
+    ), (
+        new_pattern(r"([+\-*/!@#$%&|:<>=?~^]+)"),
+        TokenKind::Special,
+    ), (
+        new_pattern(r"[,;]"),
+        TokenKind::Delimiter,
+    ), (
+        new_pattern(r"[()\[\]{}]"),
+        TokenKind::Brace,
+    )]
+}
+
+fn apply_pattern(
+    pattern: Regex,
+    kind: TokenKind,
+    tokens: &mut Tokens,
+    input: &str,
+) -> Option<String> {
+    let capture = pattern.captures(input)?;
+    let full = &capture[0];
+    tokens.push(Token {
+        kind: kind.clone(),
+        value: full.to_string(),
+    });
+    return Some(input[full.len()..].to_string());
+}
+
+fn apply_patterns(
+    patterns: &Patterns,
+    tokens: &mut Tokens,
+    input: &str,
+) -> Option<String> {
+    for (pattern, kind) in patterns {
+        let result = apply_pattern(
+            pattern.clone(),
+            kind.clone(),
+            tokens,
+            input,
+        );
+        match result {
+            None => continue,
+            some @ Some(_) => return some,
+        }
+    }
+    None
+}
+
+pub fn lex(mut input: String) -> Option<Tokens> {
+    let patterns = get_lex_patterns();
+    let mut tokens = vec![];
+    input = input.trim_start().to_string();
+
+    while !input.is_empty() {
+        input = apply_patterns(
+            &patterns,
+            &mut tokens,
+            &input
+        )?;
+        input = input.trim_start().to_string();
+    }
+
+    Some(tokens)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,249 @@
-use regex::Regex;
+use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::{iter::Peekable, vec::IntoIter};
+use std::process::Command;
+mod lexer;
 
 #[derive(Debug, Clone)]
-enum TokenKind {
-    Number,
-    String,
-    Boolean,
-    Nil,
-    Name,
+struct ValueMacro {
+    name: String,
+    tokens: lexer::Tokens,
 }
 
 #[derive(Debug, Clone)]
-struct Token {
-    kind: TokenKind,
-    value: String,
+struct FuncMacro {
+    name: String,
+    params: Vec<String>,
+    tokens: lexer::Tokens,
 }
 
-type Patterns = Vec<(Regex, TokenKind)>;
-type Tokens = Vec<Token>;
+fn replace_tokens(
+    tokens: lexer::Tokens,
+    old: lexer::Tokens,
+    new: lexer::Tokens,
+) -> lexer::Tokens {
+    let mut new_tokens = Vec::new();
+    let mut iter = tokens.into_iter().peekable();
 
-fn get_lex_patterns() -> Patterns {
-    vec![(
-        Regex::new(r"^-?\d+(\.\d+)?").unwrap(),
-        TokenKind::Number,
-    ), (
-        Regex::new(r#"^"([^"\\]|\\.)*""#).unwrap(),
-        TokenKind::String,
-    ), (
-        Regex::new(r"^(true|false)").unwrap(),
-        TokenKind::Boolean,
-    ), (
-        Regex::new(r"^(nil)").unwrap(),
-        TokenKind::Nil,
-    ), (
-        Regex::new(r"^([a-zA-Z_]\w*)").unwrap(),
-        TokenKind::Name,
-    )]
-}
-
-fn apply_pattern(
-    pattern: Regex,
-    kind: TokenKind,
-    tokens: &mut Tokens,
-    input: &str,
-) -> Option<String> {
-    let capture = pattern.captures(input)?;
-    let full = &capture[0];
-    tokens.push(Token {
-        kind: kind.clone(),
-        value: full.to_string(),
-    });
-    return Some(input[full.len()..].to_string());
-}
-
-fn apply_patterns(
-    patterns: &Patterns,
-    tokens: &mut Tokens,
-    input: &str,
-) -> Option<String> {
-    for (pattern, kind) in patterns {
-        let result = apply_pattern(
-            pattern.clone(),
-            kind.clone(),
-            tokens,
-            input,
-        );
-        match result {
-            None => continue,
-            some @ Some(_) => return some,
+    while let Some(_) = iter.peek() {
+        if iter.clone()
+                .take(old.len())
+                .zip(&old)
+                .all(|(t, o)| t.kind == o.kind && t.value == o.value) {
+            for _ in 0..old.len() {
+                iter.next();
+            }
+            new_tokens.extend(new.clone());
+        } else {
+            new_tokens.push(iter.next().unwrap());
         }
+    }
+
+    new_tokens
+}
+
+fn process_value_macro(
+    iter: &mut Peekable<IntoIter<lexer::Token>>,
+    value_macros: &mut Vec<ValueMacro>,
+    name: lexer::Token,
+) -> Option<()> {
+    let value = iter.clone().take_while(|tok|
+        tok.kind != lexer::TokenKind::EndDefine
+    );
+    for _ in 0..value.clone().collect::<Vec<_>>().len() {
+        iter.next();
+    }
+    iter.next().filter(|end| {
+        end.kind == lexer::TokenKind::EndDefine
+    })?;
+    value_macros.push(ValueMacro {
+        name: name.value,
+        tokens: value.collect(),
+    });
+    Some(())
+}
+
+fn parse_func_params_rest(
+    iter: &mut Peekable<IntoIter<lexer::Token>>,
+    args: &mut Vec<String>,
+) -> Option<()> {
+    while let Some(next_token) = iter.peek() {
+        if next_token.value.as_str() == ")" {
+            iter.next();
+            break;
+        }
+        if next_token.kind != lexer::TokenKind::Delimiter {
+            return None;
+        }
+        iter.next()?;
+        if let Some(name_token) = iter.next() {
+            if name_token.kind != lexer::TokenKind::Name {
+                return None;
+            }
+            args.push(name_token.value);
+        }
+    }
+    Some(())
+}
+
+fn parse_func_params(
+    iter: &mut Peekable<IntoIter<lexer::Token>>,
+) -> Option<Vec<String>> {
+    let mut args = vec![];
+    let name = iter.next().filter(|name|
+        name.kind == lexer::TokenKind::Name
+    )?;
+    args.push(name.clone().value);
+    parse_func_params_rest(iter, &mut args)?;
+    Some(args)
+}
+
+fn process_func_macro(
+    iter: &mut Peekable<IntoIter<lexer::Token>>,
+    func_macros: &mut Vec<FuncMacro>,
+    name: lexer::Token,
+) -> Option<()> {
+    let params = parse_func_params(iter)?;
+    iter.next().filter(|eq|
+        eq.value.as_str() == "="
+    )?;
+    let value = iter.clone().take_while(|tok|
+        tok.kind != lexer::TokenKind::EndDefine
+    );
+    for _ in 0..value.clone().collect::<Vec<_>>().len() {
+        iter.next();
+    }
+    iter.next().filter(|end| {
+        end.kind == lexer::TokenKind::EndDefine
+    })?;
+    func_macros.push(FuncMacro {
+        name: name.value,
+        params: params,
+        tokens: value.collect(),
+    });
+    Some(())
+}
+
+fn parse_func_arg(
+    iter: &mut Peekable<IntoIter<lexer::Token>>,
+) -> Option<lexer::Tokens> {
+    let mut arg_tokens = vec![];
+    let mut nesting_level = 0;
+    let nesters = vec!["(", "[", "{"];
+    let denesters = vec![")", "]", "}"];
+    while let Some(token) = iter.clone().peek() {
+        if nesting_level == 0 && token.kind == lexer::TokenKind::Delimiter {
+            return Some(arg_tokens);
+        }
+        iter.next()?;
+        if nesters.contains(&token.value.as_str()) {
+            nesting_level += 1;
+        }
+        if denesters.contains(&token.value.as_str()) {
+            if nesting_level == 0 {
+                return Some(arg_tokens);
+            }
+        }
+        arg_tokens.push(token.clone());
     }
     None
 }
 
-pub fn lex(mut input: String) -> Option<Tokens> {
-    let patterns = get_lex_patterns();
-    let mut tokens = vec![];
-    input = input.trim_start().to_string();
+fn parse_func_args(
+    iter: &mut Peekable<IntoIter<lexer::Token>>,
+) -> Option<Vec<Vec<lexer::Token>>> {
+    iter.next().filter(|lparen|
+        lparen.value.as_str() == "("
+    )?;
+    let mut args = vec![];
+    while let Some(token) = iter.clone().peek() {
+        if token.value.as_str() == ")" {
+            break
+        }
+        let arg = parse_func_arg(iter)?;
+        args.push(arg);
+    }
+    Some(args)
+}
 
-    while !input.is_empty() {
-        input = apply_patterns(
-            &patterns,
-            &mut tokens,
-            &input
-        )?;
-        input = input.trim_start().to_string();
+fn process_tokens(tokens: lexer::Tokens) -> Option<lexer::Tokens> {
+    let mut new_tokens = vec![];
+    let mut value_macros = vec![];
+    let mut func_macros = vec![];
+    let mut iter = tokens.into_iter().peekable();
+
+    while let Some(token) = iter.next() {
+        if token.kind == lexer::TokenKind::Define {
+            let name = iter.next().filter(|name|
+                name.kind == lexer::TokenKind::Macro
+            )?;
+            let eq_or_lparen = iter.next()?;
+            match eq_or_lparen.value.as_str() {
+                "=" => process_value_macro(&mut iter, &mut value_macros, name)?,
+                "(" => process_func_macro(&mut iter, &mut func_macros, name)?,
+                _ => return None,
+            }
+        } else if token.kind == lexer::TokenKind::Macro {
+            let value_macro_opt = value_macros.clone().into_iter().find(|val_macro|
+                val_macro.name == token.value
+            );
+            if let Some(value_macro) = value_macro_opt {
+                new_tokens.extend(value_macro.tokens);
+                continue;
+            }
+            let func_macro_opt = func_macros.clone().into_iter().find(|func_macro|
+                func_macro.name == token.value
+            );
+            if let Some(func_macro) = func_macro_opt {
+                let args = parse_func_args(&mut iter)?;
+                let params = func_macro.params.into_iter()
+                    .map(|s| lexer::Token {
+                        kind: lexer::TokenKind::Name,
+                        value: s,
+                    }).collect::<Vec<_>>();
+                let mut body = func_macro.tokens;
+                for (arg, param) in args.into_iter().zip(&params) {
+                    body = replace_tokens(body, vec![param.clone()], arg);
+                }
+                new_tokens.extend(body);
+                continue;
+            }
+            return None
+        } else {
+            new_tokens.push(token.clone());
+        }
     }
 
-    Some(tokens)
+    Some(new_tokens)
+}
+
+fn render_tokens(tokens: lexer::Tokens) -> String {
+    tokens.into_iter()
+        .fold("".to_string(), |acc, token|
+            acc + " " + &token.value
+        )
 }
 
 fn main() {
-    let input = "local var is 5 or nil";
-    let tokens = lex(input.to_string())
-        .expect("Tokenization failed");
-    tokens.into_iter()
-        .for_each(|token| println!("{:?}", token));
+    let args: Vec<String> = env::args().collect();
+    let input_path = args.get(1).expect("Usage: luaproc <filename>");
+    let mut input_file = File::open(input_path).expect("Could not open file");
+    let mut input = String::new();
+    match input_file.read_to_string(&mut input) {
+        Err(e) => eprintln!("Error: {}", e),
+        Ok(_) => {},
+    };
+    let tokens = lexer::lex(input).expect("Tokenization failed");
+    let processed = process_tokens(tokens).expect("Processing failed");
+    let string = render_tokens(processed);
+    let mut output_file = File::create("out.lua").expect("Could not create file");
+    match output_file.write_all(string.as_bytes()) {
+        Err(e) => eprintln!("Error: {}", e),
+        Ok(_) => {},
+    };
+    let _ = Command::new("stylua")
+        .arg("out.lua")
+        .output();
 }
-
 
